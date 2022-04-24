df660cb39af9edc576a6bb3901c179b4a22769e4
Ismailmooyoussef <88247287+Ismailmooyoussef@users.noreply.github.com>
2022-04-24 05:14:21 +0200

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "Bastion_Host" {
   ami             = var.bastion_host_ami
   key_name        = var.key_pair_name
-  count      = "${length(var.public_subnet)}"
+  count      = "${length(var.public_subnets)}"
   instance_type   = "t2.micro"
   subnet_id     = "${element(aws_subnet.public.*.id, count.index)}"
   security_groups = [aws_security_group.Public_Security_Group.id]
